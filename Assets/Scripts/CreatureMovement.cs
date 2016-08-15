eaf7927a8121b727e53a634ac14da6341f27b307
bunnyliao <zl1441@nyu.edu>
2016-08-15 16:04:15 -0400

    Change Scripts

---
+++ Assets/Scripts/CreatureMovement.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureMovement: MonoBehaviour 
+{
+	//Line
+	private Transform thisObject;
+	private LineRenderer lineRender;
+	private int vertexCount;
+	private float moveSpeed;
+	//Creature
+	private Transform creaturePosition;
+	private Transform targetPosition;
+	private string colorName;
+	//Direction
+	private int priorAngle;
+	private int currentAngle;
+
+
+	private int timeController;
+
+	void Start () 
+	{
+		thisObject = this.transform;
+		lineRender = this.gameObject.GetComponent<LineRenderer>();
+		vertexCount = 2;
+		lineRender.SetVertexCount (vertexCount);
+		moveSpeed = 2.0f;
+
+		creaturePosition  = this.transform.FindChild("Creature");
+		targetPosition = creaturePosition;
+		colorName = this.gameObject.name;
+
+		priorAngle = 0;
+		currentAngle = 0;
+
+		timeController = 0;
+
+		thisObject.position = creaturePosition.position;
+		lineRender.SetPosition (0, thisObject.position);
+		lineRender.SetPosition (1, thisObject.position);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timeController++;
+		if(timeController % 5 == 0)
+		{
+			if (thisObject.position.x < 1600 && thisObject.position.y < 1600) 
+			{
+				CreatureMovementTarget ();
+
+				currentAngle = (int)(Vector3.Angle(creaturePosition.position, targetPosition.position) * 100);
+				if ((priorAngle - currentAngle > 1)) 
+				{
+					vertexCount++;
+					lineRender.SetVertexCount (vertexCount);
+				}
+				priorAngle = currentAngle;
+			}
+			timeController = 0;
+
+		}
+		creaturePosition.position = Vector3.Lerp (creaturePosition.position, targetPosition.position , 1);
+		//Debug.Log (endTarget.position);
+		lineRender.SetPosition (vertexCount - 1, creaturePosition.position);
+
+	}
+
+	private void CreatureMovementTarget()
+	{
+		if (LightController._instance.LightColorName() == "White") 
+		{
+			targetPosition.position = new Vector3(creaturePosition.position.x + 
+				Mathf.Atan2( LightController._instance.LightPosition().x - creaturePosition.position.x, 
+					LightController._instance.LightPosition().y - creaturePosition.position.y) * moveSpeed * 0.1f,
+				creaturePosition.position.y + 
+				Mathf.Atan2( LightController._instance.LightPosition().y - creaturePosition.position.y, 
+					LightController._instance.LightPosition().x - creaturePosition.position.x) * moveSpeed * 0.1f,0);
+		}
+		else if(LightController._instance.LightColorName() == thisObject.name)
+		{
+			targetPosition.position = new Vector3(creaturePosition.position.x + 
+													Mathf.Atan2( LightController._instance.LightPosition().x - creaturePosition.position.x, 
+													LightController._instance.LightPosition().y - creaturePosition.position.y) * moveSpeed,
+												  creaturePosition.position.y + 
+													Mathf.Atan2( LightController._instance.LightPosition().y - creaturePosition.position.y, 
+													LightController._instance.LightPosition().x - creaturePosition.position.x) * moveSpeed,0);
+		}	
+
+	}
+		
+}
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour 
+{
+	public static LightController _instance;
+
+	public int choice;
+
+	private Transform thisObject;
+
+	//Light Color
+	private enum LIGHTCOLOR{ White, Purple, Yellow };
+	private int currentColor;
+	private string colorName;
+	private Color32 lightColor;
+	//LightMovement
+	private float moveSpeed;
+	private float extraSpeed;
+	private int moveRadius;
+	private int moveDirection;
+	private int pressController;
+
+	private int timeController;
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		thisObject = this.transform;
+		currentColor = (int)LIGHTCOLOR.White;
+		colorName = LIGHTCOLOR.White.ToString();
+		lightColor = new Color32 (255, 255, 255, 255);
+
+		moveSpeed = 0.05f;
+		extraSpeed = 0.0f;
+		moveDirection = 1;
+		moveRadius = -3;
+		pressController = 0;
+	}
+
+	void Update()
+	{	
+		timeController++;
+		LightMovement ();
+	}
+
+
+	private void LightMovement()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			moveDirection = -1;
+			extraSpeed = 0.1f;
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			moveDirection = 1;
+			extraSpeed = 0.1f;
+		}
+		else
+		{
+			extraSpeed = 0;
+		}
+
+			
+		thisObject.RotateAround(new Vector3(0,-300,0), new Vector3(0, 0, moveRadius), (moveSpeed + extraSpeed) * moveDirection);
+	
+	
+	}
+
+
+	public void ChooseColor (int _choice) 
+	{
+		currentColor = _choice;
+		switch (currentColor) 
+		{
+		case 0:
+			lightColor = new Color32 (255, 255, 255, 255);
+			colorName = LIGHTCOLOR.White.ToString ();
+	//		Debug.Log (colorName);
+			break;
+		case 1:
+			lightColor = new Color32 (124, 73, 129, 255);
+			colorName =  LIGHTCOLOR.Purple.ToString();
+			break;
+		case 2:
+			lightColor = new Color32 (120, 128, 45, 255);
+			colorName = LIGHTCOLOR.Yellow.ToString();
+			break;
+		default:
+			lightColor = new Color32 (255, 255, 255, 255);
+			colorName = LIGHTCOLOR.White.ToString();
+			break;
+		}
+		thisObject.gameObject.GetComponent<SpriteRenderer> ().color = lightColor;
+	}
+
+	public string LightColorName()
+	{
+		Debug.Log (colorName);
+		return colorName;
+	}
+
+	public Vector3 LightPosition()
+	{
+		return thisObject.position;
+	}
+}
