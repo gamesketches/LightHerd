4d42e4e44935a69da2e13d94b6153aae6da33a17
gamesketches <sam.von.ehren@gmail.com>
2016-08-16 22:39:09 -0400

    Added button light changing and created a gold object

---
+++ Assets/GoalScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player") {
+			Debug.Log("You win");
+		}
+	}
+}
