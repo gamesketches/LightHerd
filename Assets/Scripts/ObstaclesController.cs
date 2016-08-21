f6df58ee2a84ec8ae7d01adf6626cb0afd8f6434
bunnyliao <zl1441@nyu.edu>
2016-08-20 22:30:15 -0400

    Add obstacles

---
+++ Assets/Scripts/ObstaclesController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstaclesController : MonoBehaviour
+{
+	private Transform thisTransform;
+	private Transform[] obstaclesTransform = new Transform[100];
+
+	// Use this for initialization
+	void Start () 
+	{
+		thisTransform = this.transform;
+		for (int i = 0; i < thisTransform.childCount; i++) 
+		{
+			obstaclesTransform [i] = thisTransform.GetChild (i);
+			obstaclesTransform [i].gameObject.SetActive (false);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		for (int i = 0; i < thisTransform.childCount; i++) 
+		{
+			if (LightController._instance.LightColorName() == obstaclesTransform [i].name || 
+				LightController._instance.LightColorName() == "Default") 
+			{
+				obstaclesTransform [i].gameObject.SetActive (false);
+			}
+			else
+			{
+				obstaclesTransform [i].gameObject.SetActive (true);
+			}
+		}
+
+	
+	}
+}
