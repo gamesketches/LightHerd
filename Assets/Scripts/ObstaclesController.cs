bcd1d1cd90488d449b1a149abf0e8cf230e85208
gamesketches <sam.von.ehren@gmail.com>
2016-08-27 01:55:31 -0400

    Made renderers stay active when wrong light is on, but still go invisible

--- Assets/Scripts/ObstaclesController.cs
+++ Assets/Scripts/ObstaclesController.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class ObstaclesController : MonoBehaviour
 {
 	private Transform thisTransform;
-	private GameObject[] obstaclesTransform;
+	private SpriteRenderer[] obstacleRenderers;//GameObject[] obstaclesTransform;
 
 	// Use this for initialization
 	void Start () 
 	{
 		thisTransform = this.transform;
-		obstaclesTransform = GameObject.FindGameObjectsWithTag("obstacle");
-
-		foreach(GameObject obj in obstaclesTransform){
-			obj.SetActive (false);
+		GameObject[] obstaclesTransform = GameObject.FindGameObjectsWithTag("obstacle");
+		obstacleRenderers = new SpriteRenderer[obstaclesTransform.Length];
+		for(int i = 0; i < obstaclesTransform.Length; i++){
+			obstacleRenderers[i] = obstaclesTransform[i].GetComponent<SpriteRenderer>();
+			Color rendererColor = obstacleRenderers[i].color;
+			rendererColor.a = 0;
+			obstacleRenderers[i].color = rendererColor;
 		}
 	
 	}
@@ -23,14 +26,17 @@ public class ObstaclesController : MonoBehaviour
 	{
 		for (int i = 0; i < thisTransform.childCount; i++) 
 		{
-			if (LightController._instance.LightColorName() == obstaclesTransform [i].name || 
+			Color rendererColor = obstacleRenderers[i].color;
+			if (LightController._instance.LightColorName() == obstacleRenderers[i].gameObject.name || 
 				LightController._instance.LightColorName() == "Default") 
 			{
-				obstaclesTransform [i].SetActive (false);
+				rendererColor.a = 0;
+				obstacleRenderers[i].color = rendererColor;
 			}
 			else
 			{
-				obstaclesTransform [i].SetActive (true);
+				rendererColor.a = 1;
+				obstacleRenderers[i].color = rendererColor;
 			}
 		}
 
