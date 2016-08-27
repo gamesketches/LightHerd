54c706e5461a2ccefeac18286e7973658c2e089b
bunnyliao <zl1441@nyu.edu>
2016-08-27 00:22:34 -0400

    Revert "Revert "AddThirdPersonMode""
    This reverts commit 933a6fe210f563367554c4bae32f85f4e9ed5361.

---
+++ Assets/Scripts/ThirdPersonLightController.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ThirdPersonLightController : MonoBehaviour 
+{
+	public static ThirdPersonLightController _instance;
+
+	private Transform thisTransform;
+	private Transform lightTransform;
+
+	//Light Color
+	private enum LIGHTCOLOR{ Default, Purple, Yellow };
+	private int currentColor;
+	private string colorName;
+	private Color32 lightColor;
+	private Toggle purpleToggle;
+	private Toggle yellowToggle;
+	private GoalScript[] goals;
+	//LightMovement
+	private float moveSpeed;
+	private float extraSpeed;
+	private int moveRadius;
+	private int moveDirection;
+
+	//Background
+	private Transform bgTransform;
+	private Color32 bgColor;
+
+	private int timeController;
+
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		thisTransform = this.transform;
+		lightTransform = this.transform.FindChild ("Spotlight");
+
+		currentColor = (int)LIGHTCOLOR.Default;
+		colorName = LIGHTCOLOR.Default.ToString();
+		lightColor = new Color32 (255, 255, 255, 255);
+
+		moveSpeed = 0.03f;
+		extraSpeed = 0.0f;
+		moveRadius = -3;
+		moveDirection = 1;
+
+		bgTransform = this.transform.root.FindChild ("Background");
+		bgColor = new Color32(255, 255, 255, 255);
+		bgTransform.gameObject.GetComponent<SpriteRenderer> ().color = bgColor;
+
+		purpleToggle = this.transform.root.FindChild("Canvas").GetChild(0).GetComponent<Toggle>();
+		yellowToggle = this.transform.root.FindChild("Canvas").GetChild(1).GetComponent<Toggle>();
+		GameObject[] temp = GameObject.FindGameObjectsWithTag("goal");
+		goals = new GoalScript[temp.Length];
+		goals[0] = temp[0].GetComponent<GoalScript>();
+		goals[1] = temp[1].GetComponent<GoalScript>();
+
+		timeController = 0;
+	}
+
+	void Update()
+	{	
+		CheckToggles();
+		timeController++;
+		CheckWin();
+	}
+
+	private void CheckToggles()
+	{
+
+		if(purpleToggle.isOn && (!yellowToggle.isOn))
+		{
+			ChooseColor(1);
+		}
+		else if(yellowToggle.isOn && (!purpleToggle.isOn))
+		{
+			ChooseColor(2);
+		}
+		else 
+		{
+			ChooseColor(0);
+		}
+	}
+
+
+	public void ChooseColor (int _choice) 
+	{
+		currentColor = _choice;
+		switch (currentColor) 
+		{
+		case 0:
+			lightColor = new Color32 (255, 255, 255, 255);
+			colorName = LIGHTCOLOR.Default.ToString ();
+			bgColor = new Color32 (255, 255, 255, 255);
+			//		Debug.Log (colorName);
+			break;
+		case 1:
+			lightColor = new Color32 (124, 73, 129, 255);
+			colorName = LIGHTCOLOR.Purple.ToString ();
+			bgColor = new Color32 (235, 155, 155, 255);
+			break;
+		case 2:
+			lightColor = new Color32 (211, 218, 154, 255);
+			colorName = LIGHTCOLOR.Yellow.ToString();
+			bgColor = new Color32 (239, 140, 136, 255);
+			break;
+		default:
+			lightColor = new Color32 (255, 255, 255, 255);
+			colorName = LIGHTCOLOR.Default.ToString();
+			bgColor = new Color32 (255, 255, 255, 255);
+			break;
+		}
+		thisTransform.gameObject.GetComponent<SpriteRenderer> ().color = lightColor;
+		lightTransform.gameObject.GetComponent<Light> ().color = lightColor;
+		bgTransform.gameObject.GetComponent<SpriteRenderer> ().color = bgColor;
+	}
+
+	public string LightColorName()
+	{
+		Debug.Log (colorName);
+		return colorName;
+	}
+
+	public Vector3 LightPosition()
+	{
+		return thisTransform.position;
+	}
+
+	void CheckWin() 
+	{
+		foreach(GoalScript goal in goals)
+		{
+			if(!goal.winner) 
+			{
+				return;
+			}
+		}
+
+		Debug.Log("You win!");
+		Debug.Break();
+	}
+}
---
+++ Assets/Scripts/ThirdPersonObstaclesController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonObstaclesController : MonoBehaviour
+{
+	private Transform thisTransform;
+	private Transform[] obstaclesTransform = new Transform[100];
+
+	// Use this for initialization
+	void Start () 
+	{
+		thisTransform = this.transform;
+		for (int i = 0; i < thisTransform.childCount; i++) 
+		{
+			obstaclesTransform [i] = thisTransform.GetChild (i);
+			obstaclesTransform [i].gameObject.SetActive (false);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		for (int i = 0; i < thisTransform.childCount; i++) 
+		{
+			if (ThirdPersonLightController._instance.LightColorName() == obstaclesTransform [i].name || 
+				ThirdPersonLightController._instance.LightColorName() == "Default") 
+			{
+				obstaclesTransform [i].gameObject.SetActive (false);
+			}
+			else
+			{
+				obstaclesTransform [i].gameObject.SetActive (true);
+			}
+		}
+
+
+	}
+}
