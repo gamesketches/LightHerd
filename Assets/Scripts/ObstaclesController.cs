d60357f3032d444c235062c59064fe1794905048
gamesketches <sam.von.ehren@gmail.com>
2016-08-27 14:24:13 -0400

    Added in an obstacle animation

--- Assets/Scripts/ObstaclesController.cs
+++ Assets/Scripts/ObstaclesController.cs
@@ -4,19 +4,16 @@ using System.Collections;
 public class ObstaclesController : MonoBehaviour
 {
 	private Transform thisTransform;
-	private SpriteRenderer[] obstacleRenderers;//GameObject[] obstaclesTransform;
+	private Animator[] obstacleAnimators;
 
 	// Use this for initialization
 	void Start () 
 	{
 		thisTransform = this.transform;
 		GameObject[] obstaclesTransform = GameObject.FindGameObjectsWithTag("obstacle");
-		obstacleRenderers = new SpriteRenderer[obstaclesTransform.Length];
+		obstacleAnimators = new Animator[obstaclesTransform.Length];
 		for(int i = 0; i < obstaclesTransform.Length; i++){
-			obstacleRenderers[i] = obstaclesTransform[i].GetComponent<SpriteRenderer>();
-			Color rendererColor = obstacleRenderers[i].color;
-			rendererColor.a = 0;
-			obstacleRenderers[i].color = rendererColor;
+			obstacleAnimators[i] = obstaclesTransform[i].GetComponent<Animator>();
 		}
 	
 	}
@@ -26,17 +23,14 @@ public class ObstaclesController : MonoBehaviour
 	{
 		for (int i = 0; i < thisTransform.childCount; i++) 
 		{
-			Color rendererColor = obstacleRenderers[i].color;
-			if (LightController._instance.LightColorName() == obstacleRenderers[i].gameObject.name || 
+			if (LightController._instance.LightColorName() == obstacleAnimators[i].gameObject.name || 
 				LightController._instance.LightColorName() == "Default") 
 			{
-				rendererColor.a = 0;
-				obstacleRenderers[i].color = rendererColor;
+				obstacleAnimators[i].SetBool("active", false);
 			}
 			else
 			{
-				rendererColor.a = 1;
-				obstacleRenderers[i].color = rendererColor;
+				obstacleAnimators[i].SetBool("active", true);
 			}
 		}
 
